4b6764610f3611dc13dfc4b6f4ed6bf00018ee4d
pilotvollmar <mrmichaelkendall@gmail.com>
2023-02-06 11:20:30 +0000

    quick dirty fix for 504 error

--- src/lib/spotify.tsx
+++ src/lib/spotify.tsx
@@ -95,24 +95,35 @@ async function getNowPlaying() {
 			//re-do verification automatically before redeploying
 			return { isPlaying: false, error: true, status, statusText };
 		default:
-			const song = await response.json();
-			const isPlaying = song.is_playing;
-			const title = song?.item?.name;
-			const artist = song?.item?.artists
-				.map((_artist: any) => _artist.name)
-				.join(', ');
-			const album = song?.item?.album?.name;
-			const albumImageUrl = song?.item?.album?.images[0]?.url;
-			const songUrl = song?.item?.external_urls?.spotify;
+			try {
+				const song = await response.json();
+				const isPlaying = song.is_playing;
+				const title = song?.item?.name;
+				const artist = song?.item?.artists
+					.map((_artist: any) => _artist.name)
+					.join(', ');
+				const album = song?.item?.album?.name;
+				const albumImageUrl = song?.item?.album?.images[0]?.url;
+				const songUrl = song?.item?.external_urls?.spotify;
 
-			return {
-				album,
-				albumImageUrl,
-				artist,
-				isPlaying,
-				songUrl,
-				title,
-			};
+				return {
+					album,
+					albumImageUrl,
+					artist,
+					isPlaying,
+					songUrl,
+					title,
+				};
+			} catch {
+				return {
+					album: '',
+					albumImageUrl: '',
+					artist: '',
+					isPlaying: false,
+					songUrl: '',
+					title: '',
+				};
+			}
 	}
 }
 interface apiData {
